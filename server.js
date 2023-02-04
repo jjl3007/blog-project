79f2440ba1b2a9ed698ca6547b6b0b1971d7e5ef
Juan Jose Lozano Reyes <jlozano-reyes@myseneca.ca>
2023-02-03 20:48:35 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-// use express
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Juan Jose Lozano Reyes     Student ID: 165463217   Date: 2023-02-03
+*
+*  Cyclic Web App URL: ________________________________________________________
+*
+*  GitHub Repository URL: ______________________________________________________
+*
+********************************************************************************/ 
+
+
+
 const express = require('express');
 const app = express();
 const path = require('path');
-
-// use blog-service
 const blogService = require('./blog-service.js');
 
 var port = process.env.PORT || 8080;
@@ -17,18 +29,15 @@ blogService.initialize().then(() => {
         console.error(err);
     });
 
-// setup a 'route' to listen on the default url path (http://localhost)
 app.get('/about', function (req, res) {
     res.sendFile(path.join(__dirname + '/views/about.html'));
 });
 
-// redirect user to about page
 app.get('/', function (req, res) {
     res.redirect('/about');
 });
 
-// setup a route to listen on blog and get all the posts who have published == true
-// this route will return a json formatted string whose published property is true
+
 app.get("/blog", (req, res) => {
     blogService
         .getAllPosts()
@@ -41,8 +50,6 @@ app.get("/blog", (req, res) => {
         });
 });
 
-// setup a route to listen on blog and returns all posts within the posts.json file
-// this route will return a json formatted string of all posts
 app.get("/posts", (req, res) => {
     blogService
         .getPublishedPosts()
@@ -55,8 +62,6 @@ app.get("/posts", (req, res) => {
         });
 });
 
-// setup a route to listen on categories and returns all categories within the posts.json file
-// this route will return a json formatted string of all categories
 app.get("/categories", (req, res) => {
     blogService
         .getCategories()
@@ -69,7 +74,6 @@ app.get("/categories", (req, res) => {
         });
 });
 
-// no matching route, return error message 404
 app.use(function (req, res) {
     res.status(404).send("Page not found");
 });
